21731a460401bec7cb53336aa9a1e6b4ccfc81b4
Shegofa <shogofadeveloper12@gmail.com>
2024-08-30 00:00:14 +0400

    changes reducer.js

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -10,7 +10,7 @@ const initialState = {
   currentOperation: '',
 };
 
-const formatInput = input =>
+const formatInput = (input) =>
   input
     .replace(/ {2,}/g, ' ')
     .replace(/ {1}([+\-*\/]) {1}/g, ' $1 ')
